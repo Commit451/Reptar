ee51ac2bb9a47e5cb1513f61b02774b454fa884a
John Carlson <jawnnypoo@gmail.com>
2016-12-01 00:10:16 -0600

    :dragon:

---
+++ reptar/src/main/java/com/commit451/reptar/SimpleSubscriber.java
@@ -0,0 +1,10 @@
+package com.commit451.reptar;
+
+import io.reactivex.Single;
+
+/**
+ * Subscriber that only cares about on {@link }
+ */
+
+public abstract class SimpleSubscriber extends Single {
+}
