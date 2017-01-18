2b661e103c33bfc0d23148ba38bab5e47c1b5202
John Carlson <jawnnypoo@gmail.com>
2017-01-18 14:47:29 -0600

    Add support for fromIoToMainThread for Observable

--- reptar-kotlin/src/main/java/com/commit451/reptar/kotlin/reptar.kt
+++ reptar-kotlin/src/main/java/com/commit451/reptar/kotlin/reptar.kt
@@ -1,10 +1,16 @@
 package com.commit451.reptar.kotlin
 
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 fun <T> Single<T>.fromIoToMainThread(): Single<T> {
+    return this.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T> Observable<T>.fromIoToMainThread(): Observable<T> {
     return this.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
 }
